a625d7b5684fe30021b5230be35e5e36cc858671
Jax1109 <jax1@rambler.ru>
2015-10-09 23:26:59 +0300

    initial

--- src/Hometask2/Massive.java
+++ src/Hometask2/Massive.java
@@ -6,20 +6,6 @@ import java.util.Random;
  * Created by HP on 07.10.2015.
  */
 public class Massive {
-/*    public static void main(String[] args) {
-        int[][] a = new int[5][10];
-        int b = a.length;
-        for (int i = 0;i < a.length; i++)
-        {
-            for (int y = 0; y < 10; y++) {
-                a[i][y] = new Random().nextInt((99 - 10) + 1) + 10;
-                System.out.print(a[i][y] + " ");
-            }
-            System.out.println("");
-        }
-    }
-*/
-
    public static void main(String[] args) {
         int[] a = new int[10];
         int b = a.length;
@@ -28,11 +14,6 @@ public class Massive {
         {
             a[i] = new Random().nextInt((99 - 10) + 1) + 10;
             System.out.print(a[i] + " ");
-
-//            for (int y = 0; y < 10; y++) {
-//                a[i][y] = new Random().nextInt((99 - 10) + 1) + 10;
-//                System.out.print(a[i][y] + " ");
-//            }
 
         }
             System.out.println("");
