cccca0901af4e0edb19cf9a7b68240de45980184
HP <jax1@rambler.ru>
2015-10-12 02:36:00 +0300

    Merge branch 'master' of https://github.com/jax1109/QAA_courses
    # Conflicts:
    #	src/Hometask2/hometask1.java

--- src/Homework3/ht1EnumSort.java
+++ src/Homework3/ht1EnumSort.java
@@ -1,7 +1,23 @@
 package Homework3;
 
+import java.util.Collections;
+
 /**
  * Created by Jax on 10.10.15.
  */
 public class ht1EnumSort {
+    public static void main(String[] args) {
+        private enum Week {
+            WEEKDAY1("Monday"),
+            WEEKDAY2("Tuesday"),
+            WEEKDAY3("Wednesday"),
+            WEEKDAY4("Thursday"),
+            WEEKDAY5("Firday"),
+            WEEKDAY6("Suturday"),
+            WEEKDAY7("Sunday");
+
+
+        }
+        System.out.println(Collections.sort(Week));
+    }
 }
